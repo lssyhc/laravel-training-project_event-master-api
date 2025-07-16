3f9ce760ff0fd8d9dd9d6ade95ff57f29e299d68
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-17 01:11:20 +0700

    fix: set default role to 'user' in userresource if not provided

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -18,7 +18,7 @@ class UserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
-            'role' => $this->role,
+            'role' => $this->role ?? 'user',
             'events_count' => $this->whenCounted('events'),
             'events' => EventResource::collection($this->whenLoaded('events')),
             'created_at' => $this->created_at
