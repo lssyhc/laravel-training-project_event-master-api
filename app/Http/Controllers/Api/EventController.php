3c438a5549c93c4a5c0561eec790049ffd140c31
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-17 19:40:23 +0700

    fix: uncomment authorization check in store method of eventcontroller

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -37,7 +37,7 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        // Gate::authorize('create', Event::class);
+        Gate::authorize('create', Event::class);
         $validated = $request->validate([
             'name' => 'required|string|max:255',
             'description' => 'required|string',
